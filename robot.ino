0515937512edd2942ceffaaeec547f6d4be2e729
Hugo Dubois <195347@ecam.be>
2023-02-22 09:44:56 +0100

    (#1) stepper sequence

--- robot.ino
+++ robot.ino
@@ -16,23 +16,6 @@ void setup() {
 
 }
 
-/*
-    stepper.set_direction_spin(HIGH);
-
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-        // in steps of 1 degree
-        servo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-       servo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(15);                       // waits 15ms for the servo to reach the position
-    } 
-    // serv.rotate(90);
-    stepper.rotate_n(10,2);
-
-    delay(4000);
-*/
 
 void loop() {
 
